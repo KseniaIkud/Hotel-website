18f4d0900e40c6c0e8dedc93f1f34478dc59222d
Ksenia <kudriavtseva_k@mail.ru>
2020-06-21 10:24:56 +0300

    css mixin refactoring, common class 'description' removed

--- form_elements/dist/dropdown-guests-default.ad838450.js
+++ form_elements/dist/dropdown-guests-default.ad838450.js
@@ -269,7 +269,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44589" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45461" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
