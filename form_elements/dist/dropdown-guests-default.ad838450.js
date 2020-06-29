06c6cb37741b4efaa1abceb238b5306ba143d191
Ksenia <kudriavtseva_k@mail.ru>
2020-06-29 15:00:59 +0300

    rich checkbox buttons added

--- form_elements/dist/dropdown-guests-default.ad838450.js
+++ form_elements/dist/dropdown-guests-default.ad838450.js
@@ -269,7 +269,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43341" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44387" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
