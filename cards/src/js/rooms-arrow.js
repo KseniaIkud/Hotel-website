052e5651bcc4ae59e40f56144e937be4061cbf04
Ksenia <kudriavtseva_k@mail.ru>
2020-07-09 13:21:45 +0300

    cards page completed

--- cards/src/js/rooms-arrow.js
+++
@@ -1,8 +0,0 @@
-const rooms = document.querySelectorAll('.room-card');
-rooms.forEach(room=> {
-    room.addEventListener('click', 
-    function() {
-        let arrows = this.querySelector('.room-card__arrows');
-        arrows.classList.toggle('hidden');
-    })
-} );
