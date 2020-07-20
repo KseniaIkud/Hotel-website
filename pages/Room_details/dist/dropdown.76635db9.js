8ffdcd70c8be328b368804a25bac46315534a719
Ksenia <kudriavtseva_k@mail.ru>
2020-07-20 13:36:05 +0300

    room details page completed

--- pages/Room_details/dist/dropdown.76635db9.js
+++ pages/Room_details/dist/dropdown.76635db9.js
@@ -155,7 +155,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44053" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33445" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
