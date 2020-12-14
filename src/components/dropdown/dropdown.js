50d494e243430ba9136f3dff316a9750873e2241
Ksenia <kudriavtseva_k@mail.ru>
2020-12-15 00:01:30 +0300

    dropdown styles added

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,6 +1,7 @@
 import './images/expand.svg';
 $(function() {
     $('.js-guest__expand').on('click', function() {
+        $(this).toggleClass('js-guest_expand')
         const dropdown = $(this).closest('.js-guest').find('.js-guest-dropdown')
         dropdown.toggleClass('js-guest_open')
     })
